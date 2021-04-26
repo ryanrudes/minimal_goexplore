3cd1d6b5ea09afcdbe5cca6c167fa4e579296d7a
Ryan Rudes <ryanrudes@gmail.com>
2021-04-26 12:26:21 -0400

    Delete goexplore directory

--- goexplore/powers.py
+++
@@ -1,4 +0,0 @@
-class Powers:
-    times_chosen = 0.5
-    times_chosen_since_new = 0.5
-    times_seen = 0.5
--- goexplore/weights.py
+++
@@ -1,4 +0,0 @@
-class Weights:
-    times_chosen = 0.1
-    times_chosen_since_new = 0
-    times_seen = 0.3
